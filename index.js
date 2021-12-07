473c03f219bf2e78fb7ce74ce6558e2b2afcb9fe
Taito Udagawa <taito.97222@gmail.com>
2021-12-07 13:53:23 +0900

    localstorageに配置

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let todoElement = ""; // html要素
+let todoElement = "";
 // listオブジェクトからhtml要素を作成
 let makeTodoList = (list) => {
   list.forEach((element) => {
     todoElement = todoElement + "<li>" + element["title"] + "</li>"
     if ("subList" in element) {
-      todoElement = todoElement + "<ul>"
+      todoElement = todoElement + "<ul>";
       makeTodoList(element["subList"]);
-      todoElement = todoElement + "</ul>"
+      todoElement = todoElement + "</ul>";
     }
   })
 };
@@ -15,12 +15,11 @@ let maketemplate = () => {
   let template = JSON.stringify({"version":"1.00","list":[]});
   localStorage.setItem('todoList', template);
 }
-
+// localstorageチェック
 let todoList = localStorage.getItem("todoList");
 console.log(todoList);
-// localstorageチェック
 if (todoList) {
-  makeTodoList(todoList);
+  makeTodoList(todoList["list"]);
   $('#todoList').append(todoElement);
 } else {
   maketemplate();
