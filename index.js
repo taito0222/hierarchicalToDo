ca0c1ae028a1c427cca2be9d10581c39ed7025eb
Taito Udagawa <taito.97222@gmail.com>
2021-12-07 13:46:58 +0900

    localstorageに配置

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-let todoElement = "";
-//
-$.ajax({
-  type: "GET",
-  url: "sample.json", // ファイルパス（相対パス）
-  dataType: "json", // ファイル形式
-  async: false // 非同期通信フラグ
-}).then(
-  function (json) {
-    let obj = JSON.stringify(json);
-    let reflexive = (list) => {
-      list.forEach((element) => {
-        todoElement = todoElement + "<li>" + element["title"] + "</li>"
-        if ("subList" in element) {
-          todoElement = todoElement + "<ul>"
-          reflexive(element["subList"]);
-          todoElement = todoElement + "</ul>"
-        }
-      });
-    };
-    localStorage.setItem('todoList', obj);
-    let todoList = json["list"]
-    reflexive(todoList);
-    $('#todoList').append(todoElement);
-  },
-  function () {
-    console.log("読み込みに失敗しました");
-  }
-);
+let todoElement = ""; // html要素
+// listオブジェクトからhtml要素を作成
+let makeTodoList = (list) => {
+  list.forEach((element) => {
+    todoElement = todoElement + "<li>" + element["title"] + "</li>"
+    if ("subList" in element) {
+      todoElement = todoElement + "<ul>"
+      makeTodoList(element["subList"]);
+      todoElement = todoElement + "</ul>"
+    }
+  });
+};
+// todoList テンプレート作成
+let maketemplate = () => {
+  let template = JSON.stringify({"version":"1.00","list":[]});
+  localStorage.setItem('todoList', template);
+}
+
+let todoList = localStorage.getItem("todoList");
+console.log(todoList);
+// localstorageチェック
+if (todoList) {
+  makeTodoList(todoList);
+  $('#todoList').append(todoElement);
+} else {
+  maketemplate();
+}
+
 $('#inputButton').on('click', function() {
   var target = '<li>' + $('#inputToDo').val() + '</li>';
   $('ul').append(target);
